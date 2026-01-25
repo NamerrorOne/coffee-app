0cd4ab65405ae62306adee7b0a2893f095679cf8
Igor <igor14abramov@gmail.com>
2026-01-25 22:50:33 +0300

    homework 2

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome to the Coffee App!</Text>
+      <Text style={styles.title}>Одно из самых вкусных кофе в городе!</Text>
+      <Text style={styles.subtitle}>
+        Свежие зёрна, настоящая арабика и бережная обжарка
+      </Text>
+      <Button title="Начать" />
     </View>
   );
 }
@@ -12,8 +16,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    paddingBottom: 20,
+    paddingLeft: 30,
+    paddingRight: 30,
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "flex-end",
+  },
+  title: {
+    fontFamily: "Sora",
+    fontWeight: 600,
+    fontSize: 34,
+    letterSpacing: 1,
+    textAlign: "center",
+    color: "black",
+    marginBottom: 8,
+  },
+  subtitle: {
+    color: "#A9A9A9",
+    fontFamily: "Sora",
+    fontWeight: 400,
+    fontStyle: "normal",
+    fontSize: 14,
+    letterSpacing: 1,
+    textAlign: "center",
   },
 });
